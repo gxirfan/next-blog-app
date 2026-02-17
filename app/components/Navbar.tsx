d698d1159b8f89bba6c8565df9474b6b20f8a464
gxirfan <gxirfan@gmail.com>
2026-02-17 15:19:43 +0100

    icon update for alpha-beta versions.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -108,8 +108,18 @@ const Navbar = () => {
           </div>
 
           <Link href="/" className="group flex items-center">
-            <span className="text-xl font-black tracking-tighter text-cyan-500 group-hover:text-cyan-400 transition-all duration-300">
+            <span className="text-xl font-black tracking-tighter text-cyan-500 group-hover:text-cyan-400 transition-all duration-300 flex">
               {process.env.NEXT_PUBLIC_PROJECT_NAME || "Blog"}
+              {process.env.NEXT_PUBLIC_VERSION_TEST === "alpha" && (
+                <span className="text-sm font-extrabold text-red-500 flex items-start justify-end">
+                  Alpha
+                </span>
+              )}
+              {process.env.NEXT_PUBLIC_VERSION_TEST === "beta" && (
+                <span className="text-sm font-extrabold text-yellow-500 flex items-start justify-end">
+                  Beta
+                </span>
+              )}
             </span>
           </Link>
         </div>
